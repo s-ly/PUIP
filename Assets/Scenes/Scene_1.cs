116cba080317fb144b5f2b86aadbbe7f8ccaa497
s-ly <sergeylysov@outlook.com>
2021-09-21 00:14:09 +0300

    0.3

--- Assets/Scenes/Scene_1.cs
+++ Assets/Scenes/Scene_1.cs
@@ -6,9 +6,45 @@ using UnityEngine;
 
 public class Scene_1 : MonoBehaviour
 {
+    // детали
+    public GameObject mesh; // пробка
+    public GameObject otherMesh; // другие детали
+
+    public int flagHideMesh = 1;
+    public int flagHideOtherMesh = 1;
+
     // Выход из приложения.
     public void ExitApp()
     {
         Application.Quit();
+    }
+
+    // скрыть пробку
+    public void HideMeshProbka()
+    {
+        if (flagHideMesh == 1)
+        {
+            mesh.SetActive(false);
+            flagHideMesh = 0;
+        }
+        else
+        {
+            mesh.SetActive(true);
+            flagHideMesh = 1;
+        }
+    }
+    // скрыть детали
+    public void HideOtherMesh()
+    {
+        if (flagHideOtherMesh == 1)
+        {
+            otherMesh.SetActive(false);
+            flagHideOtherMesh = 0;
+        }
+        else
+        {
+            otherMesh.SetActive(true);
+            flagHideOtherMesh = 1;
+        }
     }
 }
